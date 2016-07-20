adcbbfeffbd3cff2d7c7dad67ecd6031293ab01a
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2016-07-20 01:34:23 -0700

    Set CSRF correctly on login instead of logut

--- generators/app/templates/jhipster/_auth.session.service.js
+++ generators/app/templates/jhipster/_auth.session.service.js
@@ -8,6 +8,7 @@
     AuthServerProvider.$inject = ['$http', '$localStorage', '$ionicHistory', 'Config'<% if (enableWebsocket) { %>, 'JhiTrackerService'<% } %>];
 
     function AuthServerProvider ($http, $localStorage, $ionicHistory, Config<% if (enableWebsocket) { %>, JhiTrackerService<% } %>) {
+        var numError = 0;
         var service = {
             getToken: getToken,
             hasValidToken: hasValidToken,
@@ -39,7 +40,17 @@
                     'Content-Type': 'application/x-www-form-urlencoded'
                 }
             }).success(function (response) {
+                numError = 0;
+                $http.post(Config.ENV.SERVER_URL + 'api/account').error(function (data, code, headers) {
+                    $localStorage['X-CSRF-TOKEN'] = headers('x-csrf-token-ionic');
+                });
                 return response;
+            }).error(function (error, code, headers) {
+                numError += 1;
+                $localStorage['X-CSRF-TOKEN'] = headers('x-csrf-token-ionic');
+                if (numError < 2) {
+                    login(credentials);
+                }
             });
         }
 
@@ -55,12 +66,6 @@
                         $localStorage['X-CSRF-TOKEN'] = headers('x-csrf-token-ionic');
                     });
                     return response;
-                })
-                .error(function (response, code, headers) {
-                    if (code === 403) {
-                        $localStorage['X-CSRF-TOKEN'] = headers('x-csrf-token-ionic');
-                        logout();
-                    }
                 });
 
         }
